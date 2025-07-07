ae920131681001e8b795a842ab6e4e77771f61b1
marensovich <droniku2009@mail.ru>
2025-07-08 02:46:38 +0300

    Базовая обработка команды /settings

--- src/main/java/org/marensovich/Bot/CommandsManager/Commands/SettingsCommand.java
+++ src/main/java/org/marensovich/Bot/CommandsManager/Commands/SettingsCommand.java
@@ -1,7 +1,10 @@
 package org.marensovich.Bot.CommandsManager.Commands;
 
 import org.marensovich.Bot.CommandsManager.Command;
+import org.marensovich.Bot.TelegramBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class SettingsCommand implements Command {
     @Override
@@ -11,6 +14,13 @@ public class SettingsCommand implements Command {
 
     @Override
     public void execute(Update update) {
-
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId().toString());
+        sendMessage.setText("Команда в разработке");
+        try {
+            TelegramBot.getInstance().execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
